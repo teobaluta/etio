6d51a06c520059fb684493761ab24704b95cc920
Teodora Baluta <teobaluta@gmail.com>
2022-09-04 00:23:41 +0000

    Clean-up and comments for numbers in Fig.1 and 2 (unadjusted effects)

--- analyze/statistical_analysis.py
+++ analyze/statistical_analysis.py
@@ -24,6 +24,7 @@ TREATMENT_VARS = {
     'MLLeakLAcc': [x for x in FEATURES if x not in ['CentroidDistance.sorted_3.']],
 }
 
+# For example, dataset path is ../aggregated_stats/full_summary.csv
 dataset = sys.argv[1]
 attack = 'Oak17Acc'
 wd = 0.0005
@@ -68,50 +69,38 @@ cluster2 = view5.loc[(view5['NumParams'] <= 1)
                 & (view5['Scheduler?'] == 'with_scheduler')
                  ]
 # This shows that the relationship is indeed positive
-cluster3 = view5.loc[(view5['NumParams'] <= 1)
-                & (view5['NumParams'] > 0.02)
-                & (view5['Dataset'] == 'cifar10')
-                & (view5['Arch'] == 'alexnetwol')
-                & (view5['TrainSize'] == 1.0)
-                & (view5['Scheduler?'] == 'with_scheduler')]
-
+# Results in Fig. 1 in the paper - for illustration purposes of the
+# possible confounding effects when looking at slices of data
 plt.scatter(cluster1['AccDiff'], cluster1['Oak17Acc'])
-#plt.savefig('fig1-confounders.png')
-#plt.close()
 plt.scatter(cluster2['AccDiff'], cluster2['Oak17Acc'])
 plt.xlabel('The Train-to-test Accuracy Gap')
 plt.ylabel('Multiple Shadow Model Attack Accuracy')
 plt.savefig('fig1-confounders.png')
-#plt.scatter(cluster3['AccDiff'], cluster3['Oak17Acc'])
-#plt.savefig('fig2-confounders.png')
 pd.set_option('display.max_rows', None)
 
-# Check the average treatment effect
+# Check the average treatment effect for all of the experiments for the
+# multipleshadow model attack
+# Computes the non-adjusted effects E1 and E3 (as shown in Fig. 2 in the paper)
+# For the adjusted effects see the answer_queries.py script
 pd.crosstab(df.Oak17Acc, df.AccDiff, margins=True, normalize='columns')['All'].sum()
 max_accdiff = max(df.AccDiff)
 min_accdiff = min(df.AccDiff)
 maxdiff_cond = df.loc[(np.abs(df['AccDiff'] - max_accdiff) < 0.001)]
 mindiff_cond = df.loc[(np.abs(df['AccDiff'] - min_accdiff) < 0.001)]
 cond_avg = maxdiff_cond['Oak17Acc'].mean() - mindiff_cond['Oak17Acc'].mean()
-print('Estimated conditional expectation E[Attack|AccDiff=min->max] {}'.format(cond_avg))
+print('Estimated conditional expectation E[Attack|AccDiff=max] - ' \
+      'E[Attack|AccDiff=min] = {} (E1)'.format(cond_avg))
+print('\tE[Attack|AccDiff=max] = {} ; '\
+      'E[Attack|AccDiff=min] = {}'.format(maxdiff_cond['Oak17Acc'].mean(),
+                                          mindiff_cond['Oak17Acc'].mean()))
 
 max_numparams = df['NumParams'].max()
 min_numparams = df['NumParams'].min()
 mindiff_cond = df.loc[(np.abs(df['NumParams'] - min_numparams) < 0.001)]
 maxdiff_cond = df.loc[(np.abs(df['NumParams'] - max_numparams) < 0.001)]
-mindiff_cond['Oak17Acc'].mean()
-maxdiff_cond['Oak17Acc'].mean()
 cond_avg = maxdiff_cond['Oak17Acc'].mean() - mindiff_cond['Oak17Acc'].mean()
-print('Estimated conditional expectation E[Attack|NumParams=min->max] {}'.format(cond_avg))
-
-# mindiff_cond = df.loc[(np.abs(df['NumParams'] - min_numparams) < 0.0001)]
-# maxdiff_cond = df.loc[(np.abs(df['NumParams'] - max_numparams) < 0.0001)]
-# maxdiff_cond = df.loc[(np.abs(df['AccDiff'] - max_accdiff) < 0.001)]
-
-# df.loc[(np.abs(df['AccDiff'] - 0.2) < 0.001)]
-# df.loc[(np.abs(df['AccDiff'] - 0.2) < 0.1)]
-# df.loc[(np.abs(df['AccDiff'] - 0.1) < 0.1)]
-# df.loc[(np.abs(df['AccDiff'] - 0.1) < 0.01)]
-# around01 = df.loc[(np.abs(df['AccDiff'] - 0.1) < 0.01)]
-# around01[['NumParams','AccDiff','Oak17Acc']]
-# print(around01)
+print('Estimated conditional expectation E[Attack|NumParams=max] - '
+      'E[Attack|NumParams=min] = {} (E3)'.format(cond_avg))
+print('\tE[Attack|NumParams=max] = {} ; ' \
+      'E[Attack|NumParams=min] = {}'.format(maxdiff_cond['Oak17Acc'].mean(),
+                                            mindiff_cond['Oak17Acc'].mean()))
